72ed8e80dee6d03eb0f4f7b14524a02a000e6323
unknown <spencer@.(none)>
2011-10-29 16:30:01 -0600

    dont force me to comment

---
+++ src/com/stejskal/web/scala/fantasydrafter/models/FantasyTeam.scala
@@ -0,0 +1,5 @@
+package com.stejskal.web.scala.fantasydrafter.models
+
+case class FantasyTeam(name: String, owner: String, nick: String)
+{
+}
--- web/static/javascript/Move.js
+++ web/static/javascript/Move.js
@@ -1,1 +1,26 @@
-function moveLeft(){ $j('#MyTeam').toggleClass('collapsed');}
+var popoutOptions = ["#MyTeam", "#PickList", "#DraftHistory"];
+
+function popoutTabClicked(clicked)
+{
+    if ($(clicked).hasClass('hidden'))
+    {
+        $(clicked).removeClass('hidden');
+        $(clicked + "Tab").removeClass('hidden');
+        for (var i = 0; i < popoutOptions.length; i++)
+        {
+            if (popoutOptions[i] != clicked)
+            {
+                $(popoutOptions[i]).addClass('hidden');
+                $(popoutOptions[i] + "Tab").addClass('hidden');
+            }
+        }
+    }
+    else
+    {
+        for (var i = 0; i < popoutOptions.length; i++)
+        {
+            $(popoutOptions[i]).addClass('hidden');
+            $(popoutOptions[i] + "Tab").removeClass('hidden');
+        }
+    }
+}
